b8e5f83a40c2e04fb030085888ae49ab8c34f5e5
weiyu02 <weiyu02@megvii.com>
2025-09-17 20:26:04 +0800

    init

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import duck.*;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Duck mallard = new MallardDuck();
+        mallard.performQuack();
+        mallard.performFly();
+    }
+}
