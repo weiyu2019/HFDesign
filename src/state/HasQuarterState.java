f808b106c31105452a5207aac5434bde551f1e26
weiyu02 <weiyu02@megvii.com>
2025-09-19 15:25:18 +0800

    init

---
+++ src/state/HasQuarterState.java
@@ -0,0 +1,26 @@
+package state;
+
+public class HasQuarterState implements  State{
+    GumballMachine gumballMachine;
+    public HasQuarterState(GumballMachine gumballMachine)
+    {
+        this.gumballMachine = gumballMachine;
+    }
+    public void insertQuarter()
+        {
+            System.out.println("You can't insert another quarter");
+        }
+        public void ejectQuarter()
+        {
+            System.out.println("Quarter returned");
+            gumballMachine.setState(gumballMachine.getNoQuarterState());
+        }
+        public void turnCrank()
+        { 
+        }
+        public void dispense()
+        {
+            System.out.println("No gumball dispensed");
+        }
+        
+}
